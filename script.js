f1bb5dc7a55d6f5fdd9513fbd6ab28e6b2200bd2
Sanjula <sanjula8@gmail.com>
2020-02-03 01:26:27 -0600

    beginning of javascript file, can't get my console.log to work on browswer

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
 // DOM Elements
+var apiKey = "&appid=43b150c9d098f7c4ffc1080bdaa12675";
 
-// Attach Weather API
+// Store value of input
+let city = $("#searchTerm").val();
 
-// Call Hidden List
+let date = new Date();
 
-// Call Hidden Forecase
+// Search Button Keypress:
+$("#searchTerm").keypress(function(event) {
+	if (event.keyCode === 13) {
+		event.preventDefault();
+		$("#searchBtn").click();
+	}
+});
 
-// Call Hidden Weather Display
+// Search Button On-Function:
+
+$("#searchBtn").on("click", function() {
+	var city = "Dallas,US";
+	// var city = $("#searchCity").val();
+
+	// // Clear Search Box
+	// $("#searchCity").val("");
+
+	// Attach Weather API for Current City
+	var queryURL =
+		"https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	})
+		// We store all of the retrieved data inside of an object called "response"
+		.then(function(response) {
+			// Log the queryURL
+			console.log(queryURL);
+
+			// Log the resulting object
+			console.log(response);
+
+			// console.log(response.name);
+			// console.log(response.weather[0].icon);
+
+			// let tempF = (response.main.temp - 273.15) * 1.8 + 32;
+			// console.log(Math.floor(tempF));
+
+			// console.log(response.main.humidity);
+
+			// console.log(response.wind.speed);
+
+			// getCurrentConditions(response);
+			// getCurrentForecast(response);
+			// makeList();
+		});
+});
